db836c51aa22de23b28bb553b594b808c85ec02b
Hail Lim <limhail@msu.edu>
2025-04-29 22:31:26 -0500

    improved UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,8 +4,9 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Ha-il Lim Portfolio</h1>
+      <h1>Ha-il (Ian) Lim Portfolio</h1>
       <p>Computer Science Student & Developer</p>
+      <img src="/profile.jpg" alt="Profile" className="profile-image" />
     </header>
   );
 }
